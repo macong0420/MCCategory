67962c039c539cee71af57eac1a587e32bb94a85
macongcong <macongcong@zongheng.com>
2017-04-26 11:36:04 +0800

    添加了新代码- 设置 tableViewHeaderView 下拉放大

--- MCCategory/ViewController.m
+++ MCCategory/ViewController.m
@@ -23,8 +23,20 @@
     btn.frame = CGRectMake(KSCALE*100, KSCALE*100, MCWidthScale(100), MCHeightScale(50));
     [btn setTitle:@"dianji" forState:UIControlStateNormal];
     [btn mc_setBackgroundColor:MCColor(@"#00bf99", 1.0) forState:UIControlStateHighlighted];
-    [self.view addSubview:btn];
+//    [self.view addSubview:btn];
     [btn addTarget:self action:@selector(btnClick) forControlEvents:UIControlEventTouchUpInside];
+    
+    UILabel *lab = [[UILabel alloc] init];
+    lab.frame = CGRectMake(100, 100, 100, 50);
+    [self.view addSubview:lab];
+    lab.backgroundColor = MCColor(@"#aaaaaa", 1.0);
+    lab.text = @"斜体字体测试";
+    
+    CGAffineTransform matrix =CGAffineTransformMake(1, 0, tanf(25 * (CGFloat)M_PI / 180), 1, 0, 0);//设置反射。倾斜角度。
+    
+    UIFontDescriptor *desc = [ UIFontDescriptor fontDescriptorWithName :[UIFont systemFontOfSize:14].fontName matrix:matrix];//取得系统字符并设置反射。
+    lab.font = [UIFont fontWithDescriptor:desc size :14];
+    
 }
 
 - (void)btnClick
